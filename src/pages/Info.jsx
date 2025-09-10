8bdb4340dc1380f9527cf5c8a4729e48815ce247
DeraTheOz <emmanuelchi545@gmail.com>
2025-09-10 21:41:36 +0100

    add Info and Plans pages

---
+++ src/pages/Info.jsx
@@ -0,0 +1,13 @@
+import SideBar from "../components/Sidebar";
+import Form from "../components/Form";
+
+function Info() {
+  return (
+    <>
+      {/* <SideBar /> */}
+      <Form />
+    </>
+  );
+}
+
+export default Info;
---
+++ src/pages/Plans.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+function Plan() {
+  return (
+    <>
+      {/* <Sidebar /> */}
+      <p>Select your plan</p>
+    </>
+  );
+}
+
+export default Plan;
