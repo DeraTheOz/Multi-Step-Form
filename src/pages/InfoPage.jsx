2ff13234e8b9f186c9f28dc0058fb6904cdac572
DeraTheOz <emmanuelchi545@gmail.com>
2025-09-24 09:35:17 +0000

    delete redundant comments

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -24,45 +24,6 @@ function InfoPage() {
     }
   }
 
-  /*
-  function validateForm() {
-    const name = fullName.trim().replace(/\s+/g, " ");
-    const normalizedPhone = phoneNumber.replace(/[\s\-()]/g, "");
-
-    const nameRegex = /^[\p{L}][\p{L}\p{M}' .-]{1,149}$/u;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-    const phoneRegex = /^\+?[0-9]{7,15}$/;
-
-    const newErrors = {
-      fullName: !name
-        ? "This field is required"
-        : !nameRegex.test(name)
-        ? "Must be at least 2 letters"
-        : null,
-      emailAddress: !emailAddress
-        ? "This field is required"
-        : !emailRegex.test(emailAddress)
-        ? "Invalid email address"
-        : null,
-      phoneNumber: !phoneNumber
-        ? "This field is required"
-        : !phoneRegex.test(normalizedPhone)
-        ? "At least 11 digits"
-        : null,
-    };
-    dispatch(setErrors(newErrors));
-
-    // Return true if NO errors
-    return !Object.values(newErrors).some((error) => error !== null);
-  }
-
-  function handleNext() {
-    if (validateForm()) {
-      navigate("/plan");
-    }
-  }
-  */
-
   return (
     <>
       <div className="bg-white shadow-primary rounded-[0.63rem] -mt-16 flex flex-col justify-center px-4 py-10 row-start-2 col-start-1 col-end-4 mx-2 lg:shadow-none lg:rounded-none lg:row-start-1 lg:col-start-2 lg:m-0 lg:p-0 lg:pt-10 lg:mx-16">
