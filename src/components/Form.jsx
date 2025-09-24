1660ebba01551119a7baf677a7ef357b0c9fead6
DeraTheOz <emmanuelchi545@gmail.com>
2025-09-24 09:39:09 +0000

    feat: add state from redux to control form inputs and update the state in the store

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,58 +1,90 @@
+import { useDispatch, useSelector } from "react-redux";
+import { handleFormUpdate } from "../features/user-info/userInfoSlice";
+
 function Form() {
+  const dispatch = useDispatch();
+  const { fullName, emailAddress, phoneNumber, errors } = useSelector(
+    (store) => store.userInfo
+  );
+
+  function handleChange(e) {
+    const { id, value } = e.target;
+    dispatch(handleFormUpdate({ inputField: id, value })); // Update form state in the store
+  }
+
   return (
     <form className="flex flex-col mt-6 lg:w-[28rem]">
       <div className="space-y-4">
         <div>
           <label
-            htmlFor="name"
+            htmlFor="fullName"
             className="md:text-sm flex justify-between mb-2 text-xs"
           >
             <span>Name</span>
-            <span className="text-strawberry-red font-bold">
-              This field is required
-            </span>
+            {errors.fullName && (
+              <span className="text-strawberry-red font-bold">
+                {errors.fullName}
+              </span>
+            )}
           </label>
           <input
             type="text"
-            id="name"
+            id="fullName"
+            value={fullName}
+            onChange={handleChange}
             placeholder="Vanessa Mint"
-            className="border-light-gray text-[0.9375rem] md:text-base  placeholder-shown:text-marine-blue focus:border-purplish-blue focus:outline-none w-full h-12 px-4 font-medium bg-white border rounded-[0.25rem] md:rounded-lg cursor-pointer"
+            className={`${
+              errors.fullName && "border-strawberry-red"
+            } border-light-gray text-[0.9375rem] md:text-base  placeholder-shown:text-marine-blue focus:border-purplish-blue focus:outline-none w-full h-12 px-4 font-medium bg-white border rounded-[0.25rem] md:rounded-lg cursor-pointer`}
           />
         </div>
 
         <div>
           <label
-            htmlFor="email"
+            htmlFor="emailAddress"
             className="md:text-sm flex justify-between mb-2 text-xs"
           >
             <span>Email Address</span>
-            <span className="text-strawberry-red font-bold">
-              This field is required
-            </span>
+            {errors.emailAddress && (
+              <span className="text-strawberry-red font-bold">
+                {errors.emailAddress}
+              </span>
+            )}
           </label>
           <input
             type="email"
-            id="email"
+            id="emailAddress"
+            value={emailAddress}
+            onChange={handleChange}
             placeholder="vanessamint@gmail.com"
-            className="border-light-gray text-[0.9375rem] md:text-base placeholder-shown:text-marine-blue focus:border-purplish-blue focus:outline-none h-12 rounded-[0.25rem] md:rounded-lg w-full px-4 font-medium bg-white border cursor-pointer"
+            className={`${
+              errors.emailAddress && "border-strawberry-red"
+            } border-light-gray text-[0.9375rem] md:text-base placeholder-shown:text-marine-blue focus:border-purplish-blue focus:outline-none h-12 rounded-[0.25rem] md:rounded-lg w-full px-4 font-medium bg-white border cursor-pointer`}
           />
         </div>
 
         <div>
           <label
-            htmlFor="number"
+            htmlFor="phoneNumber"
             className="md:text-sm flex justify-between mb-2 text-xs"
           >
             <span>Phone Number</span>
-            <span className="text-strawberry-red font-bold">
-              This field is required
-            </span>
+            {errors.phoneNumber && (
+              <span className="text-strawberry-red font-bold">
+                {errors.phoneNumber}
+              </span>
+            )}
           </label>
           <input
-            type="number"
-            id="number"
+            type="tel"
+            id="phoneNumber"
+            inputMode="numeric"
+            value={phoneNumber}
+            onChange={handleChange}
             placeholder="e.g +1 234 567 890"
-            className="border-light-gray text-[0.9375rem] md:text-base placeholder-shown:text-marine-blue focus:border-purplish-blue focus:outline-none h-12 rounded-[0.25rem] md:rounded-lg w-full px-4 font-medium bg-white border cursor-pointer"
+            className={`${
+              errors.phoneNumber && "border-strawberry-red"
+            } border-light-gray text-[0.9375rem] md:text-base placeholder-shown:text-marine-blue focus:border-purplish-blue focus:outline-none h-12 rounded-[0.25rem] md:rounded-lg w-full px-4 font-medium bg-white border cursor-pointer`}
           />
         </div>
       </div>
