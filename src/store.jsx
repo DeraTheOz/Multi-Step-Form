ba6ead87ef9064ae072fb82167ec9c77ee9aefcc
DeraTheOz <emmanuelchi545@gmail.com>
2025-09-26 23:52:21 +0000

    refactor: refactor store and add reducer for resetting store state

--- src/store.jsx
+++ src/store.jsx
@@ -1,15 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import userInfoReducer from "./features/user-info/userInfoSlice";
 import plansReducer from "./features/plans/planSlice";
 import addOnsReducer from "./features/addOns/addOnSlice";
 
+const appReducer = combineReducers({
+  userInfo: userInfoReducer,
+  plan: plansReducer,
+  addOns: addOnsReducer,
+});
+
+// Root reducer for resetting state
+const rootReducer = (state, action) => {
+  if (action.type === "reset") {
+    return appReducer(undefined, action); // reset entire store state
+  }
+  return appReducer(state, action);
+};
+
+// Configure store with root reducer
 const store = configureStore({
-  reducer: {
-    userInfo: userInfoReducer,
-    plan: plansReducer,
-    addOns: addOnsReducer,
-  },
+  reducer: rootReducer,
 });
 
 export default store;
