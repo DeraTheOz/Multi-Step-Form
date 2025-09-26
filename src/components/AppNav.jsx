50c018f4eaa5ed8a51b421fcc865527e2220eccd
DeraTheOz <emmanuelchi545@gmail.com>
2025-09-26 21:33:44 +0000

    disable clicking on nav links

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -9,7 +9,10 @@ function AppNav() {
       <ul className="h-[10.75rem] pr-[0.03rem] pb-[0.08rem] pt-9 flex justify-center text-sm font-bold text-white uppercase lg:h-auto lg:p-0 lg:mt-9 lg:ml-9 lg:flex lg:flex-col lg:gap-6 lg:mr-12">
         {navLinks.map((link, index) => (
           <li key={index}>
-            <NavLink to={path[index]} className="flex items-center">
+            <NavLink
+              to={path[index]}
+              className="flex items-center pointer-events-none"
+            >
               <span
                 className="w-7 h-7 flex items-center justify-center mr-3 border border-white rounded-full"
                 aria-hidden="true"
