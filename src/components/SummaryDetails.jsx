9e469c04ab23dc267ee6db3d8d60c1b9af637841
DeraTheOz <emmanuelchi545@gmail.com>
2025-09-26 23:56:06 +0000

    refactor: import state from store to calculate totals and add function to handle plan change

--- src/components/SummaryDetails.jsx
+++ src/components/SummaryDetails.jsx
@@ -1,39 +1,77 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+
 function SummaryDetails() {
+  const navigate = useNavigate();
+
+  // Get state from store
+  const { billing, planSelected } = useSelector((state) => state.plan);
+  const { addOnSelected } = useSelector((state) => state.addOns);
+
+  if (!billing || planSelected.length === 0) return;
+
+  const planPrice = planSelected.billing[billing];
+  const addOnsTotal = addOnSelected.reduce((acc, addOn) => {
+    return acc + addOn.billing[billing];
+  }, 0);
+  const totalAmount = planPrice + addOnsTotal;
+
+  function handlePlanChange() {
+    navigate("/plan");
+  }
+
   return (
     <>
       <div className="bg-magnolia text-light-gray flex flex-col px-4 lg:px-6 py-6 mt-6 space-y-4 rounded-lg lg:w-[28rem]">
         <div className="flex items-center justify-between">
           <div>
             <h3 className="text-marine-blue lg:text-base text-sm font-medium">
-              Arcade (Monthly)
+              {planSelected.type} ({billing[0].toUpperCase() + billing.slice(1)}
+              )
             </h3>
-            <p className="underline-offset-auto decoration-auto decoration-solid hover:text-purplish-blue text-sm underline cursor-pointer">
+            <p
+              className="underline-offset-auto decoration-auto decoration-solid hover:text-purplish-blue text-sm underline cursor-pointer"
+              onClick={handlePlanChange}
+            >
               Change
             </p>
           </div>
 
-          <p className="text-marine-blue font-bold">$9/mo</p>
+          <p className="text-marine-blue font-bold">
+            ${planSelected.billing[billing]}/
+            {billing === "monthly" ? "mo" : "yr"}
+          </p>
         </div>
 
-        <span className="bg-light-gray w-full h-[0.0625rem] opacity-[0.2043]"></span>
-
         {/* Selected Add-ons */}
-        <ul className="flex flex-col justify-between space-y-2">
-          <li className="flex items-center justify-between text-sm">
-            Online service <span className="text-marine-blue">+$1/mo</span>
-          </li>
+        {addOnSelected.length !== 0 && (
+          <>
+            <span className="bg-light-gray w-full h-[0.0625rem] opacity-[0.2043]"></span>
 
-          {/* Dummy markup for testing */}
-          <li className="flex items-center justify-between text-sm">
-            Larger storage <span className="text-marine-blue">+$1/mo</span>
-          </li>
-        </ul>
+            <ul className="flex flex-col justify-between space-y-2">
+              {addOnSelected.map((addOn) => (
+                <li
+                  key={addOn.type}
+                  className="flex items-center justify-between text-sm"
+                >
+                  {addOn.type}
+                  <span className="text-marine-blue">
+                    +${addOn.billing[billing]}/
+                    {billing === "monthly" ? "mo" : "yr"}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
 
       <div className="lg:px-6 flex items-center justify-between h-12 px-4 py-4 mt-5">
-        <p className="text-light-gray text-sm">Total (per month)</p>
+        <p className="text-light-gray text-sm">
+          Total (per {billing === "monthly" ? "month" : "year"})
+        </p>
         <p className="text-purplish-blue lg:text-xl text-base font-bold">
-          +$12/mo
+          ${totalAmount}/{billing === "monthly" ? "mo" : "yr"}
         </p>
       </div>
     </>
