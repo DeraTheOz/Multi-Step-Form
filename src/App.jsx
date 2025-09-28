bf2660517a3307b0ae62bb1a4656c815c6613a09
DeraTheOz <emmanuelchi545@gmail.com>
2025-09-28 13:04:39 +0000

    feat: added a PageNotFound route and update routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AddOnsPage from "./pages/AddOnsPage";
 import SummaryPage from "./pages/SummaryPage";
 import HomePage from "./pages/HomePage";
 import InfoPage from "./pages/InfoPage";
+import PageNotFound from "./pages/PageNotFound";
 import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
@@ -19,11 +20,12 @@ function App() {
             </ProtectedRoute>
           }
         >
-          <Route index element={<InfoPage />}></Route>
-          <Route path="plan" element={<PlanPage />}></Route>
-          <Route path="add-ons" element={<AddOnsPage />}></Route>
-          <Route path="summary" element={<SummaryPage />}></Route>
+          <Route index element={<InfoPage />} />
+          <Route path="plan" element={<PlanPage />} />
+          <Route path="add-ons" element={<AddOnsPage />} />
+          <Route path="summary" element={<SummaryPage />} />
         </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
